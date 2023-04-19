fae735d285365b64441634f276dccba7c1c7c6da
Jonathan Le <lejon1220@gmail.com>
2023-04-18 20:21:25 -0700

    adding structure to backend

---
+++ Server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3000;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
