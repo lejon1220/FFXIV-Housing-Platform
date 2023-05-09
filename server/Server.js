9f8905f2c4de4de4a131d4d6198eb18e7f01dc0d
Jonathan Le <lejon1220@gmail.com>
2023-05-09 00:34:56 -0700

    added html page

--- server/Server.js
+++ server/Server.js
@@ -5,9 +5,11 @@ const path = require('path');
 const port = 3000;
 
 
+app.use(express.json());
 
-
-
+app.get('/', (req,res) => {
+	return res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 
 
